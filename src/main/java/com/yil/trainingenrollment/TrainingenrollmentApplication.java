09b9f374f43b96752628e4c91ab3d490da6140c0
Mrityunjay-w <Mrityunjaysingh.off@gmail.com>
2024-08-23 11:39:41 +0530

    Updated War

--- src/main/java/com/yil/trainingenrollment/TrainingenrollmentApplication.java
+++ src/main/java/com/yil/trainingenrollment/TrainingenrollmentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class TrainingenrollmentApplication{
+public class TrainingenrollmentApplication extends SpringServletContainerInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingenrollmentApplication.class, args);
